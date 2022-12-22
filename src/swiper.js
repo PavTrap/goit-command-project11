d56dca659e4ca91eb0059e03127c0ebd75613bf1
Gotsman Roman <gotsmanroman@gmail.com>
2022-12-22 07:29:01 +0200

    Додає гілку customer-reviews

---
+++ src/swiper.js
@@ -0,0 +1,24 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  speed: 800,
+  loop: true,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
