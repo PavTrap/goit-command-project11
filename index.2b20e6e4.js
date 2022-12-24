bf71456cdf0dc8e67e64132366242982ef559c7e
PavTrap <105560877+PavTrap@users.noreply.github.com>
2022-12-24 14:31:57 +0000

    Deploying to gh-pages from @ PavTrap/goit-command-project11@b01d8ee669cf84d13c8005ed6dcd873f7fa77acc 🚀

--- index.2b20e6e4.js
+++
@@ -1,2 +0,0 @@
-var swiper=new Swiper(".swiper",{direction:"horizontal",speed:800,loop:!0,spaceBetween:30,autoplay:{delay:5e3,disableOnInteraction:!1,pauseOnMouseEnter:!0},pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
-//# sourceMappingURL=index.2b20e6e4.js.map
